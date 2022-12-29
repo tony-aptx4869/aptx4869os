ac6eb411c6d9fb60f83005af31f3afa960bbc403
Tony Chang <tonychang7869@gmail.com>
2022-12-30 04:46:49 +0800

    Update

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include "vgastr.h"
 int main()
 {
-    printf("Welcome to APTX4869 OS!\n");
-    printf("\n");
-    printf("Build Date & Time: December 30, 2022 04:45:00 CST (GMT+8)\n");
-    printf("\n");
-    printf("Copyright (c) 2022 Tony Chang aptx4869.tv All Rights Reserved.\n");
+    printf("Welcome to APTX4869 OS!\r\n");
+    printf("\r\n");
+    printf("Build Date & Time: December 30, 2022 04:45:00 CST (GMT+8)\r\n");
+    printf("\r\n");
+    printf("Copyright (c) 2022 Tony Chang aptx4869.tv All Rights Reserved.\r\n");
     //printf("Welcome to APTX4869 OS!");
     return 0;
 }
